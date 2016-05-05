8ce84dfdc9ce561a9d2618d76ab50076985b8e20
Brian <blittle@csumb.edu>
2016-05-04 17:39:10 -0700

    get_data function now returns views, likes, dislikes and subscribers in ints. Also, I turned the beginning part of our code into 2 new functions so we are able to call it multiple times and retrieve data for multiple channels if necessary.

--- p3.py
+++ p3.py
@@ -2,35 +2,48 @@ from bs4 import BeautifulSoup
 import re
 import urllib2
 
-channel_name = raw_input('Enter a channel name: ')
+def choose_channel():
+	channel_name = raw_input('Enter a channel name: ')
+	return channel_name
 
-root_url = "https://www.youtube.com"
-url = urllib2.urlopen("https://www.youtube.com/user/" + channel_name + "/videos")
+def retrieve_links(name_of_channel):
+	
+	root_url = "https://www.youtube.com"
+	url = urllib2.urlopen("https://www.youtube.com/user/" + name_of_channel + "/videos")
 
-content = url.read()
+	content = url.read()
 
-soup = BeautifulSoup(content, "html.parser")
-list_of_links = []
+	soup = BeautifulSoup(content, "html.parser")
+	list_of_links = []
 
-#searches for all 'a' tags that have "/watch" in them.
-for elem in soup.find_all('a', href=re.compile('/watch')):
-	list_of_links.append(root_url + elem['href'])
+	#searches for all 'a' tags that have "/watch" in them.
+	for elem in soup.find_all('a', href=re.compile('/watch')):
+		list_of_links.append(root_url + elem['href'])
 
-#This loop gets rid of duplicate links
-i = 0
-while i < len(list_of_links):
-   if list_of_links[i] == list_of_links[i+1]:
-       list_of_links.remove(list_of_links[i])
-       i += 1
+	#This loop gets rid of duplicate links
+	i = 0
+	while i < len(list_of_links):
+	   if list_of_links[i] == list_of_links[i+1]:
+	       list_of_links.remove(list_of_links[i])
+	       i += 1
 
-print("Links for the %d most recent videos:" % len(list_of_links))
+	print("Links for the %d most recent videos:" % len(list_of_links))
 
-for x in range(len(list_of_links)):
-	print(list_of_links[x])
+	for x in range(len(list_of_links)):
+		print(list_of_links[x])
+
+	return list_of_links
 
 
 #This function will go to each link and scrape data of subscribers, views, likes and dislikes
 def get_data(list_of_links = []):
+	
+	data = {}
+	data['total_views'] = 0
+	data['total_likes'] = 0
+	data['total_dislikes'] = 0
+	data['total_subscribers'] = 0
+	
 
  	for x in range(len(list_of_links)):
 
@@ -38,21 +51,38 @@ def get_data(list_of_links = []):
  		link_content = url.read()
  		link_soup = BeautifulSoup(link_content, "html.parser")
 
+ 		print("Video number: %d" % (x+1))
  		views = link_soup.find('div', {'class' : 'watch-view-count'})
  		views = views.get_text()
- 		print(views)
+ 		views = views.replace(",", "")
+ 		views = views.replace(" views", "")
+ 		data['total_views'] += int(views)
+ 		print("%s views" % views)
 
  		likes = link_soup.find('button', {'class' : "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-like-button like-button-renderer-like-button-unclicked yt-uix-clickcard-target   yt-uix-tooltip"})
  		likes = likes.get_text()
+ 		likes = likes.replace(",", "")
+ 		data['total_likes'] += int(likes)
  		print("%s likes" % likes)
 
  		dislikes = link_soup.find('button', {'class' : "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup like-button-renderer-dislike-button like-button-renderer-dislike-button-unclicked yt-uix-clickcard-target   yt-uix-tooltip"})
  		dislikes = dislikes.get_text()
+ 		dislikes = dislikes.replace(",", "")
+ 		data['total_dislikes'] += int(dislikes)
  		print("%s dislikes" % dislikes)
  		print(" ")
  	
 	subscribers = link_soup.find('span', {'class' : "yt-subscription-button-subscriber-count-branded-horizontal yt-subscriber-count"})
 	subscribers = subscribers.get_text()
-	print("%s subscribers" % subscribers)
+	subscribers = subscribers.replace(",", "")
+	data['total_subscribers'] = int(subscribers)
 
-get_data(list_of_links)
+	return data
+
+channel1 = retrieve_links(choose_channel())
+channel2 = retrieve_links(choose_channel())
+
+channel1_data = get_data(channel1)
+channel2_data = get_data(channel2)
+print(channel1_data)
+print(channel2_data)
